4988e8e1c195083e80aad700239e6f50813ee1ee
gjmfs <mufeesmc28@gmail.com>
2024-10-26 18:40:02 +0530

    first commit

---
+++ devIcons/devIcons.js
@@ -0,0 +1,128 @@
+import aws from "./aws.png";
+import bootstrap from "./bootstrap.svg";
+import cloudflare from "./Cloudflare.svg";
+import css from "./CSS.png";
+import express from "./express.svg";
+import firebase from "./firebase.png";
+import GCP from "./GCP.png";
+import git from "./git.png";
+import github from "./github.png";
+import GoDaddy from "./GoDaddy.svg";
+import html5 from "./html5.png";
+import java from "./java.png";
+import JS from "./JS.png";
+import kotlin from "./kotlin.svg";
+import linux from "./linux.png";
+import mail from "./mail.png";
+import mongodb from "./mongodb.png";
+import Netlify from "./Netlify.svg";
+import nodejs from "./nodejs.png";
+import npm from "./npm.svg";
+import php from "./php.svg";
+import python from "./python.png";
+import react from "./react.png";
+import vite from "./vite.svg";
+import domain from "./www.png";
+
+export const devIcons = [
+  {
+    img: aws,
+    name: "Amazon Web Serice",
+  },
+  {
+    img: bootstrap,
+    name: "Bootstrap",
+  },
+  {
+    img: cloudflare,
+    name: "CLoudFlare",
+  },
+  {
+    img: css,
+    name: "CSS",
+  },
+  {
+    img: express,
+    name: "Express",
+  },
+  {
+    img: firebase,
+    name: "FireBase",
+  },
+  {
+    img: GCP,
+    name: "Google Cloud Provider",
+  },
+  {
+    img: git,
+    name: "Git",
+  },
+  {
+    img: github,
+    name: "GitHub",
+  },
+  {
+    img: GoDaddy,
+    name: "GoDaddy",
+  },
+  {
+    img: html5,
+    name: "Html5",
+  },
+  {
+    img: java,
+    name: "Java",
+  },
+  {
+    img: JS,
+    name: "JavaScript",
+  },
+  {
+    img: kotlin,
+    name: "Kotlin",
+  },
+  {
+    img: linux,
+    name: "Linux",
+  },
+  {
+    img: mail,
+    name: "E-mail API",
+  },
+  {
+    img: mongodb,
+    name: "MongoDB",
+  },
+  {
+    img: Netlify,
+    name: "Netlify",
+  },
+  {
+    img: nodejs,
+    name: "NodeJS",
+  },
+  {
+    img: npm,
+    name: "Node Package Manager",
+  },
+  {
+    img: php,
+    name: "PHP",
+  },
+  {
+    img: python,
+    name: "Python",
+  },
+  {
+    img: react,
+    name: "ReactJS",
+  },
+  {
+    img: vite,
+    name: "Vite",
+  },
+  {
+    img: domain,
+    name: "Domain register",
+  },
+];
